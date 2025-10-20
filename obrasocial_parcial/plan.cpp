33bb5b500cc233b66f0b344510c062ffe5b53a59
Patricio De Maria <108100239+pppato@users.noreply.github.com>
2025-10-20 11:05:50 -0300

    Remove commented-out GenerarArchivoPlanes method
    Removed the commented-out method for generating plan files.

--- obrasocial_parcial/plan.cpp
+++ obrasocial_parcial/plan.cpp
@@ -31,41 +31,4 @@ bool Plan::verifPrestacion(std::string p)
     return existe;
 }
 
-/*void Plan::GenerarArchivoPlanes()
-{
-    std::ofstream basico("PlanBasico.txt");
-    if (basico.is_open()) {
-        basico << "Consulta médica general\n";
-        basico << "Análisis de sangre básico\n";
-        basico << "Atención ambulatoria\n";
-        basico.close();
-        std::cout << "Archivo PlanBasico.txt generado correctamente.\n";
-    } else {
-        std::cout << "Error al crear PlanBasico.txt\n";
-    }
 
-    std::ofstream medio("PlanMedio.txt");
-    if (medio.is_open()) {
-        medio << "Consulta médica general\n";
-        medio << "Análisis de sangre completo\n";
-        medio << "Radiografía\n";
-        medio << "Electrocardiograma\n";
-        medio.close();
-       std:: cout << "Archivo PlanMedio.txt generado correctamente.\n";
-    } else {
-       std:: cout << "Error al crear PlanMedio.txt\n";
-    }
-
-   std::ofstream premium("PlanPremium.txt");
-    if (premium.is_open()) {
-        premium << "Consulta médica general\n";
-        premium << "Análisis de sangre completo\n";
-        premium << "Resonancia magnética\n";
-        premium << "Cirugía menor\n";
-        premium << "Internación completa\n";
-        premium.close();
-        std::cout << "Archivo PlanPremium.txt generado correctamente.\n";
-    } else {
-        std::cout << "Error al crear PlanPremium.txt\n";
-    }
-}*/
